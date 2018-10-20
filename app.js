1b2f1f1fb46afad6833d458038adac12f24f8f20
James Hicks <james@adnetinc.com>
2018-10-19 17:45:16 -0700

    only team 1 currently showing

--- app.js
+++ app.js
@@ -1,14 +1,17 @@
 const config = {
     numberOfTeamsInNhl: 31,
     baseApi: 'https://statsapi.web.nhl.com//api/v1',
-    teamsApi: '/teams',
+    teamsApi: '/teams/',
     endPoints: {
         rosterApi: '/?expand=team.roster',
     }
 }
 
+let databasePlayerArray = [];
+let currentNumber = 1;
+
 let getTeamRoster = {
-    path: `${config.baseApi}${config.teamsApi}${config.endPoints.rosterApi}`
+    path: `${config.baseApi}${config.teamsApi}${currentNumber}${config.endPoints.rosterApi}`
     ,
     params: {
         rosterApi: config.endPoints.rosterApi
@@ -16,15 +19,11 @@ let getTeamRoster = {
 };
 
 function consoleLog(callback) {
+    console.log(i);
     console.log(callback);
 }
 
-fetchJSON(getTeamRoster, consoleLog)
-    // .then(function (response) {
-    //     debugger;
-    //     return response.json();
-    // })
-    // .then(function (myJson) {
-    //     console.log(JSON.stringify(myJson));
-    // });
-
+for (i = 1; i < config.numberOfTeamsInNhl; i++) {
+    currentNumber++;
+    fetchJSON(getTeamRoster, consoleLog)
+}
